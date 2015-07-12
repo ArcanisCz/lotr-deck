345b0556ff11b2c1e2c161f57f191a369a17fc69
Arcanis <arcaniscz@gmail.com>
2015-07-12 20:56:20 +0200

    Connection Success!

--- app/src/main/java/cz/arcanis/lotr/config/AppConfig.java
+++ app/src/main/java/cz/arcanis/lotr/config/AppConfig.java
@@ -1,6 +1,6 @@
 package cz.arcanis.lotr.config;
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.vaadin.spring.annotation.EnableVaadin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,31 +21,16 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 })
 public class AppConfig {
 
-//    @Bean
-//    public MongoDbFactory mongo() throws Exception {
-//        MongoCredential.createPlainCredential("reader1", "pokus", "pokus".toCharArray());
-//        MongoClient client = new MongoClient("ds059712.mongolab.com", 59712);
-////        MongoClient client = new MongoClient("127.8.179.130", 27017);
-//        MongoDbFactory factory = new SimpleMongoDbFactory(client, "lotr");
-////        MongoClient mongo = new MongoClient("127.8.179.130", 27017);
-//
-//        return factory;
-//    }
-//
-//    @Bean
-//    public MongoTemplate mongoTemplate() throws Exception {
-//        MongoTemplate template = new MongoTemplate(mongo());
-//        return template;
-//    }
 
     @Bean
-    public Mongo mongo() throws Exception {
-//        return new Mongo("ds059712.mongolab.com", 59712);
-        return new Mongo("127.8.179.130", 27017);
+    public MongoClient mongo() throws Exception {
+        return new MongoClient("localhost", 27000);
+//        return new MongoClient("127.8.179.130", 27017);
     }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "lotr", new UserCredentials("reader1", "pokus"));
+//        return new MongoTemplate(mongo(), "lotr", new UserCredentials("reader1", "pokus"));
+        return new MongoTemplate(mongo(), "lotr");
     }
 }
