b49e0272eb07bfd0214f5355593a9497c612473a
Arcanis <arcaniscz@gmail.com>
2015-07-13 02:08:29 +0200

    deploy production mode

--- app/src/main/java/cz/arcanis/lotr/config/AppConfig.java
+++ app/src/main/java/cz/arcanis/lotr/config/AppConfig.java
@@ -1,12 +1,15 @@
 package cz.arcanis.lotr.config;
 
 import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
 import com.vaadin.spring.annotation.EnableVaadin;
 import cz.arcanis.lotr.model.entity.converter.CardReadConverter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -26,19 +29,37 @@ import java.util.Arrays;
         "cz.arcanis.lotr.vaadin",
         "cz.arcanis.lotr.backend"
 })
+@PropertySource(value = "classpath:properties.properties")
 public class AppConfig {
 
+    @Value("${mongo.host}")
+    private String mongoHost;
+
+    @Value("${mongo.port}")
+    private String mongoPort;
+
+    @Value("${mongo.user}")
+    private String mongoUser;
+
+    @Value("${mongo.password}")
+    private String mongoPassword;
+
+    @Value("${mongo.db}")
+    private String mongoDb;
 
     @Bean
     public Mongo mongo() throws Exception {
-        return new Mongo("localhost", 27000);
-//        return new Mongo("127.8.179.130", 27017);
+        return new Mongo(mongoHost, Integer.parseInt(mongoPort));
     }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        MongoTemplate template = new MongoTemplate(mongo(), "lotr");
-//        MongoTemplate template = new MongoTemplate(mongo(), "lotr", new UserCredentials("reader2", "pokus"));
+        MongoTemplate template;
+        if(StringUtils.isBlank(mongoUser) && StringUtils.isBlank(mongoPassword)){
+            template= new MongoTemplate(mongo(), mongoDb);
+        }else{
+            template = new MongoTemplate(mongo(), mongoDb, new UserCredentials(mongoUser, mongoPassword));
+        }
 
         CustomConversions customConversions = new CustomConversions(Arrays.asList(new Converter[]{new CardReadConverter()}));
         MappingMongoConverter converter = (MappingMongoConverter) template.getConverter();
@@ -47,5 +68,10 @@ public class AppConfig {
         converter.afterPropertiesSet();
 
         return template;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
